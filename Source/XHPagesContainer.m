d51dcfb977fdc2e87aa4a0489a3b212eb6dee742
Jack <xhzengAIB@gmail.com>
2014-01-23 20:16:01 +0000

    Firstly init project

---
+++ Source/XHPagesContainer.m
@@ -0,0 +1,38 @@
+//
+//  XHPagesContainer.m
+//  XHPagesContainer
+//
+//  Created by 曾 宪华 on 14-1-23.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHPagesContainer.h"
+
+@interface XHPagesContainer ()
+
+@end
+
+@implementation XHPagesContainer
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
